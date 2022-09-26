a6d49d51c96f56c6ee1e2243c877366565f9f978
jinqianzhi@zmn.cn <jinqianzhi@zmn.cn>
2022-09-26 23:07:36 +0800

    增加compose常用组件的练习代码

--- app/src/main/java/com/compose/compose_demo/MainActivity.kt
+++ app/src/main/java/com/compose/compose_demo/MainActivity.kt
@@ -4,20 +4,25 @@ import android.os.Bundle
 import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,13 +40,23 @@ class MainActivity : ComponentActivity() {
             ComposedemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
                     Greeting("Android")
                 }
             }
         }
+    }
+
+
+    private val verticalGradientBrush by lazy {
+        Brush.verticalGradient(
+            colors = listOf(
+                Color.Red,
+                Color.Green,
+                Color.Yellow
+            )
+        )
     }
 
     @Composable
@@ -56,23 +71,63 @@ class MainActivity : ComponentActivity() {
              Text(text = "点击发起网络请求")
          })*/
 
-        Row() {
-            Text(
-                text = "点击发起网络请求",
+        //在compose中使用modifier的属性时，需要注意调用顺序，不同的调用顺序所显示的效果也有所不同
+        Row(
+            modifier = Modifier
+                .padding(16.dp)//设置外间隙
+                .fillMaxWidth() //宽度填满整个父控件
+                .height(150.dp)
+                .clip(shape = RoundedCornerShape(12.dp))//裁剪
+                .background(color = Color.Gray)
+                .border(width = 2.dp, color = Color.Red, shape = RoundedCornerShape(12.dp))//圆角边框
+                .padding(8.dp)//设置内间隙
+        ) {
+            Image(
+                painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
                 modifier = Modifier
-                    .clip(shape = RoundedCornerShape(8.dp))
-                    .background(color = Color.Blue)
-                    .border(2.dp, color = Color.Red, shape = RoundedCornerShape(8.dp))
-                    .padding(8.dp)
-                    .clickable {
-                        viewModel.getBannerData()
-                    },
-                style = TextStyle(color = Color.White)
+                    .size(60.dp)//将宽度和高度同时设置为60dp
+                    .clip(
+                        CircleShape
+                    )//将图片裁剪为圆形
             )
+
+            //间距组件
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Image(
+                painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(
+                        CircleShape
+                    )
+            )
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(color = Color.Red)//设置纯色背景
+            ) {
+                Text(text = "纯色", Modifier.align(Alignment.Center))
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(brush = verticalGradientBrush)//设置垂直线性渐变背景
+            ) {
+                Text(text = "渐变色", modifier = Modifier.align(Alignment.Center))
+            }
+
+
         }
     }
-
 }
+
 
 @Preview(showBackground = true)
 @Composable
